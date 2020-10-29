a9f1e998319f9bd52216aa1239492fff24655136
Zoran <sourcecode3010@gmail.com>
2020-10-29 16:53:30 +0100

    Added Category controller  and view and Layout page edited

---
+++ RockyOpet/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockyOpet.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()        // desni klik na index i add a View
+        {
+            return View();
+        }
+    }
+}
