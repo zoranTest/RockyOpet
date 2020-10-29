b176c6f59c99072775589216924fc2c03931f430
Zoran <sourcecode3010@gmail.com>
2020-10-29 16:11:02 +0100

    Add project files.

---
+++ RockyOpet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RockyOpet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
