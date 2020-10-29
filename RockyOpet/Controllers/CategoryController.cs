2deff4f2d6bbd131a4995046226c185d76bf7b34
Zoran <sourcecode3010@gmail.com>
2020-10-29 17:17:41 +0100

    Added dbcontext to CategoryC  and table  in View for Categories from DB

--- RockyOpet/Controllers/CategoryController.cs
+++ RockyOpet/Controllers/CategoryController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RockyOpet.Data;
+using RockyOpet.Models;
+
+
+// kad god nesto saljemo iz Kontrolera moramo unutar viewa to da prihvatimo
+
 
 namespace RockyOpet.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+
+        public CategoryController(ApplicationDbContext dbb)
+        {
+            _db = dbb;
+        }
+
+
+            
         public IActionResult Index()        // desni klik na index i add a View
         {
-            return View();
+
+            IEnumerable<Category> objList = _db.Category;       // pokupi sve kategorije iz baze
+
+            return View(objList);   // vratimo ovu listu da bude prikazana u viewu a u viewu imamo model koji to pokupi
         }
+
+
+
+
+
+
+
     }
 }
